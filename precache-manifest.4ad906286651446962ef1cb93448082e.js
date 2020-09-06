0908b9b6ec2dacde09528f2cf9cdc762206d06b9
LadyDeveloper <anhacorrea@hotmail.com>
2020-09-06 17:33:03 -0500

    Updates

---
+++ precache-manifest.4ad906286651446962ef1cb93448082e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa58a383ac26f43d8f33e5977e53176",
+    "url": "/marlonmanuel/index.html"
+  },
+  {
+    "revision": "a30e30df19550d8dfa15",
+    "url": "/marlonmanuel/static/css/2.d651f42e.chunk.css"
+  },
+  {
+    "revision": "d191ebe8b9a5d49f3321",
+    "url": "/marlonmanuel/static/css/main.49ab8f46.chunk.css"
+  },
+  {
+    "revision": "a30e30df19550d8dfa15",
+    "url": "/marlonmanuel/static/js/2.ed2c4e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marlonmanuel/static/js/2.ed2c4e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d191ebe8b9a5d49f3321",
+    "url": "/marlonmanuel/static/js/main.da43e3b0.chunk.js"
+  },
+  {
+    "revision": "d5fee47c951441c15f63",
+    "url": "/marlonmanuel/static/js/runtime-main.28ddbc80.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/marlonmanuel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/marlonmanuel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/marlonmanuel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/marlonmanuel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/marlonmanuel/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
