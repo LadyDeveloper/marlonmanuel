e5c3d1124f9a32a193bce219438a71839da2f171
LadyDeveloper <anhacorrea@hotmail.com>
2020-09-07 10:46:42 -0500

    Fixing master branch

---
+++ src/components/FormInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FormInfo = () => {
+  return (
+    <div className="form-info">
+      <button>
+        <i className="fa fa-info-circle fa-1x"></i>REQUEST INFO
+      </button>
+      <button>DETAILS ></button>
+    </div>
+  );
+};
+
+export default FormInfo;
